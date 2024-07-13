7587429c67a07eed6b527401492c8857b532c5b9
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-13 17:08:18 +0200

    fixed the split as strncpy started in i=-1

--- level4/ft_split.c
+++ level4/ft_split.c
@@ -4,7 +4,7 @@
 //ft_strncpy to copy n bytes from s2 to s1
 char *ft_strncpy(char *s1, char *s2, int n)
 {
-	int i = -1;
+	int i = 0;
 
 	while (i < n && s2[i])
     {
@@ -35,16 +35,17 @@ char	**ft_split(char *str)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	int wc = 0;
+	int wc = ft_count_words(str);
+
 	
     //allocate memory for the array of strings
 	char **out = (char **)malloc(sizeof(char *) * (wc + 1));
-	i = 0;
+	if(!out)
+		return 0;
 	
     //split the string into words
 	while (str[i])
 	{
-		int wc = ft_count_words(str);
         //skip the spaces and find the start of the word
 		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
 			i++;
