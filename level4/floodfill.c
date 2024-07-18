91dc03ea8a39be205b911bd9e73529894a0adb15
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-18 13:00:12 +0200

    fixed to work

--- level4/floodfill.c
+++ level4/floodfill.c
@@ -1,28 +1,72 @@
 
 //Definition of the point structure	
 
-typedef struct s_point
+typedef struct	s_point
 {
-	int	x;
-	int	y;
-}	t_point;
+	int x;
+	int y;
+} t_point;
 
-//Void function to fill the tab with -1
-
-void	fill(int **tab, t_point size, t_point begin)
+//function to fill the area with the character to_fill recursively
+void	fill(char **tab, t_point size, t_point cur, char to_fill)
 {
-	if (begin.x < 0 || begin.y < 0 || begin.x >= size.x 
-		|| begin.y >= size.y || tab[begin.y][begin.x] != 0)
-		return ;
-	tab[begin.y][begin.x] = -1;
-	fill(tab, size, (t_point){begin.x + 1, begin.y});
-	fill(tab, size, (t_point){begin.x - 1, begin.y});
-	fill(tab, size, (t_point){begin.x, begin.y + 1});
-	fill(tab, size, (t_point){begin.x, begin.y - 1});
+	//if the current point is out of bounds or the character is not the same as to_fill, return
+	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
+		return;
+	//fill the current point with the character to_fill
+	tab[cur.y][cur.x] = 'F';
+	//recursively fill the area around the current point
+	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
 }
 
-//Main function to fill the tab with -1
-void	flood_fill(int **tab, t_point size, t_point begin)
+void	flood_fill(char **tab, t_point size, t_point begin)
 {
-	fill(tab, size, begin);
+	fill(tab, size, begin, tab[begin.y][begin.x]);
+}
+
+
+//For testing purposes
+#include <stdlib.h>
+#include <stdio.h>
+
+char** make_area(char** zone, t_point size)
+{
+	char** new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+
+	return new;
+}
+
+int main(void)
+{
+	t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+
+	char**  area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point begin = {7, 4};
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	return (0);
 }
